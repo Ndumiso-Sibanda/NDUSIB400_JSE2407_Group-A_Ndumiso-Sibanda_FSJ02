c61b892397f975b11800bf72b7fcf45c722546b6
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-09-19 09:48:27 +0200

    spinner

---
+++ tumu-app/app/components/Spinner.js
@@ -0,0 +1,8 @@
+export default function Spinner() {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-16 h-16 border-8 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+  
